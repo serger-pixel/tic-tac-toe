92eadf72bed68600691cf35cd64c264dec334858
Sergey <rihler@localhost.localdomain>
2025-12-11 12:57:16 +0300

    Добавление метода возврата строки таблицы ячеек

--- table.hpp
+++ table.hpp
@@ -1,10 +1,30 @@
 #include<vector>
 #include"cell.hpp"
+enum class numbRow{
+    FIRST,
+    SECOND,
+    THIRD
+};
+
 
 class Table{
 public:
     Cell getCell(CellId id){
         return cells[static_cast<int>(id)];
+    }
+
+    std::vector<Cell> getRow(numbRow row){
+        std::vector<Cell> row = std::vector<Cell>();
+        
+        switch (row)
+        {
+        case constant expression:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
     }
 
     Table(){
